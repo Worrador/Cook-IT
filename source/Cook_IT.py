83679b21740075f94f2d6d7a4a90a3173f080ad2
Melchan <a.poleczki@gmail.com>
2024-09-15 21:27:34 +0200

    not using txt file for stroing fileID

--- source/Cook_IT.py
+++ source/Cook_IT.py
@@ -10,7 +10,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 import openpyxl
 
 SCOPES = ['https://www.googleapis.com/auth/drive.file']
-FILE_NAME = 'Recipe_list.xlsx'
+FILE_NAME = 'Recipes.xlsx'
 
 if getattr(sys, 'frozen', False):
     # If running as a frozen executable
@@ -25,7 +25,7 @@ class CookITLogic:
     def __init__(self):
         self.service = None
         self.wb = None
-        self.ws_Recipe_list = None
+        self.ws_Recipes = None
         self.ws_recency = None
         self.row_count = 0
         self.file_id = None
@@ -45,18 +45,20 @@ class CookITLogic:
         self.service = build('drive', 'v3', credentials=creds)
 
     def get_or_create_file(self):
-        # Check if we have a stored file ID
+        # Check if we have a stored file ID in the local Excel file
         print("Checking if we have a stored file ID...")
-        if os.path.exists('file_id.txt'):
-            with open('file_id.txt', 'r') as f:
-                self.file_id = f.read().strip()
-
-            # Verify the file still exists in Drive
-            try:
-                self.service.files().get(fileId=self.file_id).execute()
-                return
-            except:
-                pass  # File not found, we'll create a new one
+        if os.path.exists(FILE_NAME):
+            self.wb = openpyxl.load_workbook(FILE_NAME)
+            self.ws_Recipes = self.wb['Recipes']
+            stored_file_id = self.ws_Recipes.cell(row=1, column=6).value
+            if stored_file_id:
+                self.file_id = stored_file_id
+                # Verify the file still exists in Drive
+                try:
+                    self.service.files().get(fileId=self.file_id).execute()
+                    return
+                except:
+                    pass  # File not found, we'll create a new one
 
         # Search for the file in Drive
         results = self.service.files().list(
@@ -65,35 +67,26 @@ class CookITLogic:
         items = results.get('files', [])
 
         if not items:
-            print("Stored file ID not found. Creating a new file...")
+            print("File not found in Drive. Creating a new file...")
             # File doesn't exist, create it
-            wb = openpyxl.Workbook()
-            ws = wb.active
-            ws.title = "Recipe_list"
-            ws.append(["Recipe Name", "URL", "Comment", "Number or Recipes"])
-            wb.create_sheet("Recency")
-            wb.save(FILE_NAME)
+            self.wb = openpyxl.Workbook()
+            self.ws_Recipes = self.wb.active
+            self.ws_Recipes.title = "Recipes"
+            self.ws_Recipes.append(["Recipe Name", "URL", "Comment", "Number of Recipes"])
+            self.wb.create_sheet("Recency")
+            self.wb.save(FILE_NAME)
 
             file_metadata = {'name': FILE_NAME}
-
             with open(FILE_NAME, 'rb') as file:
                 media = MediaIoBaseUpload(file,
                                         mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
                                         resumable=True)
-
-                file_metadata = {'name': FILE_NAME}
                 file = self.service.files().create(body=file_metadata, media_body=media, fields='id').execute()
                 self.file_id = file.get('id')
                 print(f"Created new file with ID: {self.file_id}")
-                with open('file_id.txt', 'w') as f:
-                    f.write(self.file_id)
         else:
             # File exists, use the first match
             self.file_id = items[0]['id']
-
-        # Store the file ID locally
-        with open('file_id.txt', 'w') as f:
-            f.write(self.file_id)
 
     def download_file(self):
         try:
@@ -111,48 +104,38 @@ class CookITLogic:
             raise
 
     def load_workbook(self):
-        if not os.path.exists(FILE_NAME):
-            # TODO: do we need this part here? Shouldnt creating be handled by get or create method?
-            self.wb = openpyxl.Workbook()
-            self.ws_Recipe_list = self.wb.active
-            self.ws_Recipe_list.title = "Recipe_list"
-            self.ws_Recipe_list.append(["Recipe Name", "URL", "Comment", "Number or Recipes"])
-            self.ws_recency = self.wb.create_sheet("Recency")
-            self.ws_recency.append(["Recency"])
+        self.wb = openpyxl.load_workbook(FILE_NAME)
+        self.ws_Recipes = self.wb['Recipes']
+        self.ws_recency = self.wb['Recency']
+        # Get the stored row count
+        stored_count = self.ws_Recipes.cell(row=1, column=5).value
+
+        if stored_count is None or self.ws_Recipes.cell(row=stored_count, column=1).value is None:
+            # Recounting needed
             self.row_count = 1
         else:
-            self.wb = openpyxl.load_workbook(FILE_NAME)
-            self.ws_Recipe_list = self.wb['Recipe_list']
-            self.ws_recency = self.wb['Recency']
-            # Get the stored row count
-            stored_count = int(self.ws_Recipe_list.cell(row=1, column=5).value)
+            # Count only rows with data starting from stored_count
+            self.row_count = int(stored_count)
 
-            if stored_count is None or self.ws_Recipe_list.cell(row=stored_count, column=1).value is None:
-                # Recounting needed
-                self.row_count = 1
+        for row in self.ws_Recipes.iter_rows(min_row=self.row_count+1, max_col=1, values_only=True):
+            if row[0]:
+                self.row_count += 1
             else:
-                # Count only rows with data starting from stored_count
-                self.row_count = stored_count
-
-            for row in self.ws_Recipe_list.iter_rows(min_row=self.row_count+1, max_col=1, values_only=True):
-                if row[0]:
-                    self.row_count += 1
-                else:
-                    break
-            # Update the stored count
-            self.ws_Recipe_list.cell(row=1, column=5, value=self.row_count)
+                break
+        # Update the stored count
+        self.ws_Recipes.cell(row=1, column=5, value=self.row_count)
 
     def choose_recipe(self):
         if self.row_count < 2:
-            return None, None, None, None  # No Recipe_list available
+            return None, None, None, None  # No Recipes available
         while True:
             random_row_number = random.randint(2, self.row_count)
             recency_value = self.ws_recency.cell(row=random_row_number, column=1).value or 0
 
             if recency_value < random.randint(1, 100):
-                recipe_name = self.ws_Recipe_list.cell(row=random_row_number, column=1).value
-                url = self.ws_Recipe_list.cell(row=random_row_number, column=2).value
-                comment = self.ws_Recipe_list.cell(row=random_row_number, column=3).value
+                recipe_name = self.ws_Recipes.cell(row=random_row_number, column=1).value
+                url = self.ws_Recipes.cell(row=random_row_number, column=2).value
+                comment = self.ws_Recipes.cell(row=random_row_number, column=3).value
                 return recipe_name, url, comment, random_row_number
 
     def update_recency(self, chosen_row):
@@ -191,11 +174,11 @@ class CookITLogic:
             raise
 
     def add_recipe(self, name, url, comment):
-        self.ws_Recipe_list.append([name, url, comment])
+        self.ws_Recipes.append([name, url, comment])
         self.row_count += 1
-        self.ws_Recipe_list.cell(row=self.row_count, column=1, value=name)
-        self.ws_Recipe_list.cell(row=self.row_count, column=2, value=url)
-        self.ws_Recipe_list.cell(row=self.row_count, column=3, value=comment)
+        self.ws_Recipes.cell(row=self.row_count, column=1, value=name)
+        self.ws_Recipes.cell(row=self.row_count, column=2, value=url)
+        self.ws_Recipes.cell(row=self.row_count, column=3, value=comment)
         # self.ws_recency.append([0])
-        self.ws_Recipe_list.cell(row=1, column=5, value=self.row_count)
+        self.ws_Recipes.cell(row=1, column=5, value=self.row_count)
         self.save_and_upload()
