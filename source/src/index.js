916742498127d2b5183403a1184d2afd1e8858b5
Akos Poleczki <a.poleczki@gmail.com>
2024-09-28 16:51:32 +0200

    things kinda work

--- source/src/index.js
+++ source/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CookITApp from './src/CookITApp';
+import CookITApp from './CookITApp';
 
 ReactDOM.render(
   <React.StrictMode>
--- source/webpack.config.js
+++ source/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './index.js',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
@@ -10,7 +10,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/, // Match both .js and .jsx files
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
@@ -18,7 +18,17 @@ module.exports = {
             presets: ['@babel/preset-env', '@babel/preset-react']
           }
         }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader', 'postcss-loader'], // Added postcss-loader for Tailwind
       }
     ]
-  }
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'], // Allow importing .js and .jsx files without specifying the extension
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // This allows using @/ in import statements
+    },
+  },
 };
